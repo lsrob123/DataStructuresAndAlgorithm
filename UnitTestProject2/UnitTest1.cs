97dae1d8eda64436777e0512ff9f466781d0cd45
Lixin Yang <lsrob@hotmail.com>
2018-03-03 16:29:36 +1100

    1. Added unit tests

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using NUnit.Framework;
+
+namespace UnitTestProject2
+{
+    public class TypesContainer
+    {
+        public Type Type1 { get; set; }
+        public Type Type2 { get; set; }
+    }
+
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Class2
+    {
+        public int Key { get; set; }
+        public string Name { get; set; }
+    }
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var a=new TypesContainer
+            {
+                Type1= typeof(Class1),
+                Type2 = typeof(Class2),
+            };
+
+            var b = new TypesContainer
+            {
+                Type1 = typeof(Class1),
+                Type2 = typeof(Class2),
+            };
+
+            Assert.IsTrue(a.Equals(b));
+        }
+
+    }
+}
